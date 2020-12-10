d78f1d517045bd54ce0573ed578fe9a221f2e60e
CarlosBonfim <brunobonfim10@hotmail.com>
2020-12-10 14:14:47 -0400

    referenciado o agrupamento de palavras

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,20 +1,12 @@
 const { ipcMain } = require('electron')
 const pathsToRows = require('./pathsToRows')
 const prepareData = require('./prepareData')
+const groupWords = require('./groupWords')
 
 ipcMain.on('process-subtitles', (event, paths) => {
-    
+
     pathsToRows(paths)
         .then(rows => prepareData(rows))
-        .then(words => console.log(words))
-        .then(() => {
-            event.reply('process-subtitles', [
-                { name: "i", amount: 1234 },
-                { name: "you", amount: 900 },
-                { name: "he", amount: 853 },
-                { name: "she", amount: 1234 },
-                { name: "our", amount: 900 },
-                { name: "house", amount: 853 },
-            ])
-        })
+        .then(words => groupWords(words))
+        .then(groupWords => event.reply('process-subtitles', groupWords))
 }) 
