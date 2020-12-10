da01285393ad54812bcf4e630f7d5b7077ce83a5
CarlosBonfim <brunobonfim10@hotmail.com>
2020-12-10 09:54:07 -0400

    Update pathsToRows.js

--- src/backend/pathsToRows.js
+++ src/backend/pathsToRows.js
@@ -1,11 +1,8 @@
-// const { reject } = require('core-js/fn/promise')
 const fs = require('fs')
 
 module.exports = paths => {
-    console.log("aqui1")
     return new Promise((resolver, reject) => {
         try{
-            //console.log("aqui2")
             const rows = paths
                 .map(path => fs.readFileSync(path).toString('utf-8'))
                 .reduce((fullText, fileText) => `${fullText}\n${fileText}`)
